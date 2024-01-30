af2e8234c42089671bc18c4a8ee59e4d601b133d
FloCrafter <110677983+FloCrafter@users.noreply.github.com>
2024-01-30 22:56:51 +0100

    Add files via upload

--- src/main/java/de/flocrafter/swagnation2024/Swagnation2024.java
+++ src/main/java/de/flocrafter/swagnation2024/Swagnation2024.java
@@ -1,12 +1,13 @@
 package de.flocrafter.swagnation2024;
 
+import de.flocrafter.swagnation2024.Commands.Test;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Swagnation2024 extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getCommand("Test").setExecutor(new Test());
     }
 
     @Override
